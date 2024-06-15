890d16ee97780509170be0268d4ba07e3eb7b353
Kenvic001 <victorokeke64@gmail.com>
2024-06-15 07:40:25 +0100

    Kenvic001/redirect fix

--- redirect.js
+++ redirect.js
@@ -3,8 +3,8 @@
   if (window.location.protocol === 'http:') {
     const redirectTo = 'https://www.' + window.location.hostname + window.location.pathname + window.location.search;
     window.location.href = redirectTo;
-  } else if (window.location.hostname === 'aronetech.com') {
-    const redirectTo = 'https://www.aronetech.com' + window.location.pathname + window.location.search;
+  } else if (window.location.hostname === 'aroneenergy.com') {
+    const redirectTo = 'https://www.aroneenergy.com' + window.location.pathname + window.location.search;
     window.location.href = redirectTo;
   }
 })();
