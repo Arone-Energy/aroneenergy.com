1d10deed65e6f5ce6238568f91c48c49e8ca4f99
Kenvic001 <victorokeke64@gmail.com>
2025-10-23 17:30:27 +0100

    Kenvic001/Logo Fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
     <div className="flex items-center justify-between md:hidden h-[53px] px-[1rem] border-b-gray-300 border-b-[1px] bg-white ">
     
     <div className="">
-    <Link to='/'><img src={Logo} alt="" className='h-[27px]' /></Link>
+    <Link to='/'><img src={Logo} alt="" className='h-[30px]' /></Link>
      </div>
 
      <div className=" md:hidden">
